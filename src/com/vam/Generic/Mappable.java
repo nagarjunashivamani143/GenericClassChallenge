edcc32bb28008ba0a8541d42a227c617ab356586
nagarjuna <Nagarjuna.Kristam@valuemomentum.com>
2024-12-22 08:59:52 +0530

    modfied mappable with sysout

--- src/com/vam/Generic/Mappable.java
+++ src/com/vam/Generic/Mappable.java
@@ -7,9 +7,12 @@ public interface Mappable {
 	static double[] stringLocation(String location) {
 		String[] splits = location.split(",");
 		
+		System.out.println("Hi Git how are you");
+		
 		double lat = Double.valueOf(splits[0]);
 		double lon = Double.valueOf(splits[1]);
 		return new double[] {lat,lon};
+		
 		
 	}
 	
